18d3db1a74eafa197a062ad6d5c1c0be71678e6f
raholm <rasmus.holm723@gmail.com>
2017-07-12 16:40:22 +0200

    Init package.

---
+++ texmer/R/texmer.R
@@ -0,0 +1,8 @@
+#' texmer R package
+#'
+#' @description
+#' An R package for segmenting text
+#'
+#' @docType package
+#' @name texmer
+NULL
---
+++ texmer/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(texmer)
+
+test_check("texmer")

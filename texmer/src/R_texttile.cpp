fbe2a959da3eb0cbaaa43e97aaa19b9562678b9e
raholm <rasmus.holm723@gmail.com>
2017-08-03 16:41:48 +0200

    Renamed texttile_r.cpp to R_texttile.cpp and moved all R-utility function from util.h to R_texttile.cpp.

--- texmer/src/R_texttile.cpp
+++ texmer/src/R_texttile.cpp
@@ -3,6 +3,28 @@
 #include "texttile.h"
 #include "util.h"
 
+inline Corpus convert_from_R(const Rcpp::List& corpus) {
+  Corpus c;
+
+  for (auto const& document : corpus) {
+    c.push_back(Rcpp::as<Document>(document));
+  }
+
+  return c;
+}
+
+inline Document convert_from_R(const Rcpp::StringVector& document) {
+  return Rcpp::as<Document>(document);
+}
+
+inline Rcpp::StringVector convert_to_R(const DocumentSegments& segments) {
+  return Rcpp::wrap(segments);
+}
+
+inline Rcpp::List convert_to_R(const CorpusSegments& segments) {
+  return Rcpp::wrap(segments);
+}
+
 // [[Rcpp::export]]
 Rcpp::List get_texttile_segments_cpp(const Rcpp::List& tokens,
                                      const Rcpp::StringVector& stopwords,
@@ -10,9 +32,9 @@ Rcpp::List get_texttile_segments_cpp(const Rcpp::List& tokens,
                                      std::size_t block_size,
                                      const Rcpp::CharacterVector& method,
                                      bool liberal) {
-  auto texttile = TextTile(sentence_size, block_size,
-                           Rcpp::as<std::string>(method),
-                           liberal);
+  TextTile texttile{sentence_size, block_size,
+      Rcpp::as<std::string>(method),
+      liberal};
   auto segments = texttile.segment(convert_from_R(tokens),
                                    convert_from_R(stopwords));
   return convert_to_R(segments);
--- texmer/src/util.h
+++ texmer/src/util.h
@@ -1,9 +1,11 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#include <Rcpp.h>
-
 #include "def.h"
+
+#include <numeric>
+#include <algorithm>
+#include <cmath>
 
 template<typename T>
 T sum(const std::vector<T>& v) {
@@ -48,28 +50,6 @@ inline std::string to_lower(const std::string& s) {
   std::string l{s};
   std::transform(s.cbegin(), s.cend(), l.begin(), ::tolower);
   return l;
-}
-
-inline Corpus convert_from_R(const Rcpp::List& corpus) {
-  Corpus c;
-
-  for (auto const& document : corpus) {
-    c.push_back(Rcpp::as<Document>(document));
-  }
-
-  return c;
-}
-
-inline Document convert_from_R(const Rcpp::StringVector& document) {
-  return Rcpp::as<Document>(document);
-}
-
-inline Rcpp::StringVector convert_to_R(const DocumentSegments& segments) {
-  return Rcpp::wrap(segments);
-}
-
-inline Rcpp::List convert_to_R(const CorpusSegments& segments) {
-  return Rcpp::wrap(segments);
 }
 
 #endif // UTIL_H

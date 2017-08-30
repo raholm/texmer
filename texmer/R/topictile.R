5a1051a119c6f082dab2baf267bcd15f7d3a2766
raholm <rasmus.holm723@gmail.com>
2017-08-30 16:08:04 +0200

    Updated documentation.

--- texmer/R/topictile.R
+++ texmer/R/topictile.R
@@ -1,26 +1,30 @@
+#' @title
 #' Segmenting text based on Latent Dirichlet Allocation.
 #'
+#' @description
+#' description here.
+#'
+#' @details
+#' details here.
+#'
+#' @param corpus
+#' @param stopwords
+#' @param mode
+#' @param sentence_size
+#' @param block_size
+#' @param nsegments
+#' @param liberal
+#' @return A data frame with 'token' replaced by the segments in 'text'. 'id' contains the new ids and the old ids are preserved in 'docid'.
+#'
 #' @export
-tf_topictile <- function(corpus, stopwords, model, gibbs_iters=50,
+tf_topictile <- function(corpus, stopwords, mode,
                          sentence_size=20, block_size=2,
-                         n_segments=0, liberal=TRUE) {
-    .check_input_topictile(corpus, stopwords,
-                           model, gibbs_iters,
+                         nsegments=0, liberal=TRUE) {
+    .check_input_topictile(corpus, stopwords, mode,
                            sentence_size, block_size,
-                           n_segments, liberal)
+                           nsegments, liberal)
 
-    mode <- .create_mode_topictile()
-
-    for (i in 1:gibbs_iters) {
-        samples <- .generate_samples_topictile(model)
-        mode$update(samples$token, samples$topic)
-    }
-
-    tokens <- corpus %>%
-        texcur::tf_lowercase() %>%
-        texcur::tf_tokenize()
-
-    topictile_tokens <- tokens %>%
+    topictile_tokens <- corpus %>%
         dplyr::group_by(id) %>%
         dplyr::summarise(tokens=list(token)) %>%
         dplyr::select(tokens)
@@ -28,7 +32,7 @@ tf_topictile <- function(corpus, stopwords, model, gibbs_iters=50,
 
     segments <- mode$topictile(topictile_tokens, stopwords,
                                sentence_size, block_size,
-                               n_segments, liberal) %>%
+                               nsegments, liberal) %>%
         reshape2::melt() %>%
         dplyr::rename(text=value, docid=L1) %>%
         dplyr::mutate(id=as.character(row_number()),
@@ -39,38 +43,15 @@ tf_topictile <- function(corpus, stopwords, model, gibbs_iters=50,
     segments
 }
 
-.check_input_topictile <- function(corpus, stopwords,
-                                   model, gibbs_iters,
+.check_input_topictile <- function(corpus, stopwords, mode,
                                    sentence_size, block_size,
-                                   n_segments, liberal) {
-    checkr::assert_tidy_table(corpus, c("id", "text"))
+                                   nsegments, liberal) {
+    checkr::assert_tidy_table(corpus, c("id", "token"))
+    checkr::assert_character(corpus$id)
+    checkr::assert_character(corpus$token)
     checkr::assert_character(stopwords)
     checkr::assert_integer(sentence_size, len=1, lower=1)
     checkr::assert_integer(block_size, len=1, lower=1)
-    checkr::assert_integer(n_segments, len=1, lower=0)
+    checkr::assert_integer(nsegments, len=1, lower=0)
     checkr::assert_logical(liberal, len=1)
-}
-
-.get_module_topictile <- function() {
-    Module("mod_topictile", "texmer")
-}
-
-.create_mode_topictile <- function() {
-    module <- .get_module_topictile()
-    mode <- module$TypeTopicIndicatorMode
-    new(mode)
-}
-
-.generate_samples_topictile <- function(model) {
-    filename <- "./mallet_state_file_topictile.txt"
-
-    if (file.exists(filename)) file.remove(filename)
-
-    model$writeState(filename)
-    tbl <- read.table(filename)
-    names(tbl) <- c("doc", "source", "pos", "type", "token", "topic")
-
-    if (file.exists(filename)) file.remove(filename)
-
-    list(token=as.character(tbl$token), topic=as.numeric(tbl$topic))
 }
